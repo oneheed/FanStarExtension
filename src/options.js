3a53e9b1c396db24b2c277eb55e23dc0a4246f34
oneheed <oneheed@gmail.com>
2021-01-19 12:50:51 +0800

    Add Fan n Star options page

---
+++ src/options.js
@@ -0,0 +1,65 @@
+(() => {
+    let el = {
+            'dailypoint': document.getElementById('dailypoint'),
+
+            'attendance': document.getElementById('attendance'),
+            'viewArticlesKR': document.getElementById('viewArticlesKR'),
+            'viewArticlesJP': document.getElementById('viewArticlesJP'),
+            'viewArticlesCN': document.getElementById('viewArticlesCN'),
+            'viewArticlesEN': document.getElementById('viewArticlesEN'),
+            'viewArticlesVN': document.getElementById('viewArticlesVN'),
+            'sharePost': document.getElementById('sharePost'),
+
+            'attendanceNumKR': document.getElementById('attendanceNumKR'),
+            'viewArticlesNumKR': document.getElementById('viewArticlesNumKR'),
+            'viewArticlesNumJP': document.getElementById('viewArticlesNumJP'),
+            'viewArticlesNumCN': document.getElementById('viewArticlesNumCN'),
+            'viewArticlesNumEN': document.getElementById('viewArticlesNumEN'),
+            'viewArticlesNumVN': document.getElementById('viewArticlesNumVN'),
+            'sharePostNumKR': document.getElementById('sharePostNumKR'),
+
+            'bkg': chrome.extension.getBackgroundPage()
+        },
+        tasks = {
+            'attendanceNumKR': 0,
+            'viewArticlesNumKR': 0,
+            'viewArticlesNumJP': 0,
+            'viewArticlesNumCN': 0,
+            'viewArticlesNumEN': 0,
+            'viewArticlesNumVN': 0,
+            'sharePostNumKR': 0,
+        },
+        methods = {
+            'checkEvent': () => {
+                el.bkg.methods.checkEvent();
+            },
+            'attendanceEvent': () => {
+                el.bkg.methods.attendanceEvent();
+            },
+            'viewArticlesEvent': () => {
+                el.bkg.methods.viewArticlesEvent();
+            },
+            'sharePosEvent': () => {
+                el.bkg.methods.sharePosEvent();
+            },
+            'RenderNum': () => {
+                el.attendanceNumKR.textContent = tasks.attendanceNumKR;
+                el.viewArticlesNumKR.textContent = tasks.viewArticlesNumKR;
+                el.viewArticlesNumJP.textContent = tasks.viewArticlesNumJP;
+                el.viewArticlesNumCN.textContent = tasks.viewArticlesNumCN;
+                el.viewArticlesNumEN.textContent = tasks.viewArticlesNumEN;
+                el.viewArticlesNumVN.textContent = tasks.viewArticlesNumVN;
+                el.sharePostNumKR.textContent = tasks.sharePostNumKR;
+            }
+        };
+
+    // Default event listeners
+    el.dailypoint.addEventListener('click', methods.checkEvent);
+    el.attendance.addEventListener('click', methods.attendanceEvent);
+    el.viewArticlesKR.addEventListener('click', methods.Event);
+    el.viewArticlesJP.addEventListener('click', methods.Event);
+    el.viewArticlesCN.addEventListener('click', methods.Event);
+    el.viewArticlesEN.addEventListener('click', methods.Event);
+    el.viewArticlesVN.addEventListener('click', methods.Event);
+    el.sharePost.addEventListener('click', methods.sharePosEvent);
+})();
